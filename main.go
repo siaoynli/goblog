fc305445a155ed4a54612cd46306b092f763fa37
siaoynli <siaoynli@126.com>
2021-08-11 15:27:50 +0800

    ok

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Hello, 这里是 goblog</h1>")
 }
 func main() {
+
 	http.HandleFunc("/", handlerFunc)
 	http.ListenAndServe(":3000", nil)
 }
